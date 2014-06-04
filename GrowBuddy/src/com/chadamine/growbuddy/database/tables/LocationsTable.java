5cead8082f5be711c1120b50e0d222f186084d94
chadamine <chadwicklew@gmail.com>
2014-06-04 00:10:18 -0600

    new table

---
+++ GrowBuddy/src/com/chadamine/growbuddy/database/tables/LocationsTable.java
@@ -0,0 +1,24 @@
+package com.chadamine.growbuddy.database.tables;
+
+import com.chadamine.growbuddy.database.DatabaseContract.Locations;
+
+import android.database.sqlite.SQLiteDatabase;
+
+
+public class LocationsTable {
+
+		public static final String CREATE = "create table "
+		+ Locations.TABLE_NAME + "("
+		+ Locations.COL_ID + " integer primary key autoincrement, "
+		+ Locations.COL_NAME + " text not null, "
+		+ Locations.COL_LOCATION + " text not null)";
+
+		public static void onCreate(SQLiteDatabase db) {
+			db.execSQL(CREATE);
+		}
+
+		public static void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			db.execSQL("DROP TABLE IF EXISTS " + Locations.TABLE_NAME);
+			onCreate(db);
+		}
+}
