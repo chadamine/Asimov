45a9cb9bcb2173df9a04bed854c80f239cb427ad
chadamine <chadwicklew@gmail.com>
2014-06-08 00:07:02 -0600

    clean up contract

--- GrowBuddy/src/com/chadamine/growbuddy/database/DatabaseContract.java
+++ GrowBuddy/src/com/chadamine/growbuddy/database/DatabaseContract.java
@@ -26,7 +26,7 @@ public class DatabaseContract {
 	public static final class Journals implements Columns { 
 		
 		public static final String BASE_PATH = "journals";
-		public static final Uri CONTENT_URI = Uri.parse(DatabaseContract.CONTENT_URI + "/" + BASE_PATH);
+		public static final Uri CONTENT_URI = Uri.withAppendedPath(DatabaseContract.CONTENT_URI, BASE_PATH);
 		
 		// mime type of directory of items
 		public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/journals";
@@ -38,18 +38,13 @@ public class DatabaseContract {
 		public static final String COL_ID = "_id";
 		public static final String COL_NAME = "name";
 		public static final String COL_LOCATION = "location";
-	
-		//public static final Uri CONTENT_URI = 
-		/*Uri.withAppendedPath(
-				DatabaseContract.CONTENT_URI, 
-				Journals.BASE_PATH);*/
 	}
 	
 	//	Locations
 	public static final class Locations implements Columns {
 		
 		public static final String BASE_PATH = "locations";
-		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH);
+		public static final Uri CONTENT_URI = Uri.withAppendedPath(DatabaseContract.CONTENT_URI, BASE_PATH);
 		public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + BASE_PATH;
 		public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/location";
 		public static final String TABLE_NAME = "locations";
@@ -61,10 +56,6 @@ public class DatabaseContract {
 		public static final String COL_LOCATION = "location";
 		public static final String COL_BATCH = "batch";
 		public static final String COL_TYPE = "type";
-		
-		public static final Uri contentUri = Uri.withAppendedPath(
-				Locations.CONTENT_URI, 
-				Locations.BASE_PATH);
 	}
 	
 	
@@ -94,7 +85,7 @@ public class DatabaseContract {
 	public static final class Nutrients implements Columns {
 		
 		public static final String BASE_PATH = "nutrients";
-		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH);
+		public static final Uri CONTENT_URI = Uri.withAppendedPath(DatabaseContract.CONTENT_URI, BASE_PATH);
 		public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/nutrients";
 		public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/nutrient";
 		public static final String TABLE_NAME = "nutrients";
@@ -106,10 +97,6 @@ public class DatabaseContract {
 		public static final String COL_LOCATION = "location";
 		public static final String COL_BATCH = "batch";
 		public static final String COL_TYPE = "type";
-	
-		public static final Uri contentUri = Uri.withAppendedPath(
-				CONTENT_URI, 
-				BASE_PATH);
 	}
 	
 	
